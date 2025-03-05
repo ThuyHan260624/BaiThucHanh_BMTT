ee78f8979bbab812cb443e118ecc3d3c7a465194
ThuyHan260624 <thuyhan260624@gmail.com>
2025-03-05 08:05:32 +0700

    Initial commit of BaiThucHanh_BMTT

---
+++ BaiTap01.py
@@ -0,0 +1,13 @@
+def tinh_tong_chan(n):
+    tong = 0
+    for num in n:
+        if num % 2 == 0:
+            tong += num
+    return tong
+
+input_list = input("Nhập dãy số cách nhau bằng dấu cách hoặc dấu phẩy: ")
+# Replace commas with spaces
+input_list = input_list.replace(',', ' ')
+numbers = list(map(int, input_list.split()))
+tong_chan = tinh_tong_chan(numbers)
+print(f"Tổng các số chẵn trong dãy số là: {tong_chan}")
