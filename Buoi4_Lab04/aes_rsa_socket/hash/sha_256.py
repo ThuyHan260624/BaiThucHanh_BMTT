7ab4a6aa867893efd8a7b051f770d62c43103a41
ThuyHan260624 <thuyhan260624@gmail.com>
2025-03-26 11:16:55 +0700

    Delete Buoi4_Lab04 directory

--- Buoi4_Lab04/aes_rsa_socket/hash/sha_256.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def calculate_sha256_hash(data):
-    sha256_hash = hashlib.sha256()
-    sha256_hash.update(data.encode('utf-8'))
-    return sha256_hash.hexdigest()
-data_to_hash = input("Nhap vao: ")
-hash_value = calculate_sha256_hash(data_to_hash)
-print(f"SHA-256 hash of '{data_to_hash}' is {hash_value}")
